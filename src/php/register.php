92cfee0f480c8d1a907cdc1d488951aee0321586
whatswrongedu <127257647+whatswrongedu@users.noreply.github.com>
2025-06-02 23:28:14 -0300

    Creating purchase pages and fixing bugs in PHP

--- src/php/register.php
+++ src/php/register.php
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $pais = $_POST["pais"];
         $email = $_POST["email"];
         $data_nascimento = $_POST["data_nascimento"];
+        $senha_original = $_POST["senha"];
 
         // Criar o hash da senha
         $senha_hash = password_hash($senha_original, PASSWORD_DEFAULT);
@@ -39,16 +40,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ultraviolence</title>
-    <link rel="stylesheet" href="../css/register/re-main.css">
+    <link rel="stylesheet" href="../css/register-login/re-lo-main.css">
 </head>
 
 <body>
     <main>
         <h2 class="cadConcluido">Cadastro feito com sucesso!</h2>
-
+        <h3>Desfrute dessa experiência ímpar</h3>
+        <div class="successImg">
             <img src="https://66.media.tumblr.com/fd2819673aa6e9a28f5baf559d6f5b26/tumblr_pd83f69WdI1wk694yo2_r3_540.gif"
             alt="Lana smoking"/>
-
+        </div>
         <div class="btn-header">
             <a href="login.html" class="loginLink">Faça seu login</a>
         </div>
